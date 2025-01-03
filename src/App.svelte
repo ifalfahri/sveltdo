99a13246f7f8cc41108da345b289519cdfe4b001
IfalEXZ <ifaltampan69@gmail.com>
2025-01-03 21:02:12 +0800

    feat: enhance layout and styling of main component with card elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,34 @@
 <script>
 </script>
 
-<main>
-  <h1>Sveltdo</h1>
+<main class="container is-fluid">
+  <h1 class="is-size-3">Sveltdo</h1>
+  <div class="columns">
+    <div class="column is-4">
+      <div class="card has-background-light">
+        <div class="card-header">
+          <p class="card-header-title">Card</p>
+        </div>
+        <div class="card-content">
+          <div class="card mb-3 has-background-primary-light">
+            <div class="card-content">
+              <p class="has-text-primary-dark">Content</p>
+            </div>
+          </div>
+          <div class="card mb-3 has-background-primary-light">
+            <div class="card-content">
+              <p class="has-text-primary-dark">Content</p>
+            </div>
+          </div>
+          <div class="card mb-3 has-background-primary-light">
+            <div class="card-content">
+              <p class="has-text-primary-dark">Content</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
