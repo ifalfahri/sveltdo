654749584d239f7fc29f3a9f731dd16d6177cdb8
IfalEXZ <ifaltampan69@gmail.com>
2025-01-03 22:13:29 +0800

    refactor: TodoCard integration into a new CardList component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import TodoCard from "./components/TodoCard.svelte";
+  import CardList from "./components/CardList.svelte"
 
   let cards = [
     {
@@ -15,31 +15,12 @@
       list: "Task"
     },
   ]
-
-  let todo = ""
-
-  function addTodo() {
-    cards = [...cards, { todo, list: "Task"}]
-  }
 </script>
 
 <main class="container is-fluid">
   <h1 class="is-size-3">Sveltdo</h1>
   <div class="columns">
-    <div class="column is-4">
-      <div class="card has-background-light">
-        <div class="card-header">
-          <p class="card-header-title">Tasks</p>
-        </div>
-        <div class="card-content">
-          {#each cards as card}
-            <TodoCard content={card.todo} />
-          {/each}
-          <input type="text" class="input is-primary mb-2" bind:value={todo}/>
-          <button on:click={addTodo} class="button is-primary">Add</button>
-        </div>
-      </div>
-    </div>
+    <CardList cards={cards} />
   </div>
 </main>
 
---
+++ src/components/CardList.svelte
@@ -0,0 +1,25 @@
+<script>
+    import TodoCard from "./TodoCard.svelte";
+    export let cards
+
+    let todo = ""
+
+  function addTodo() {
+    cards = [...cards, { todo, list: "Task"}]
+  }
+</script>
+
+<div class="column is-4">
+    <div class="card has-background-light">
+      <div class="card-header">
+        <p class="card-header-title">Tasks</p>
+      </div>
+      <div class="card-content">
+        {#each cards as card}
+          <TodoCard content={card.todo} />
+        {/each}
+        <input type="text" class="input is-primary mb-2" bind:value={todo}/>
+        <button on:click={addTodo} class="button is-primary">Add</button>
+      </div>
+    </div>
+</div>
