59195e845d622cc97224e026ce628c22eb3cc379
IfalEXZ <ifaltampan69@gmail.com>
2025-01-03 22:30:49 +0800

    feat: implement event handling for adding todos to respective lists

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,25 @@
   let taskCards = [];
   let inProgressCards = [];
   let doneCards = [];
+
+  function handleEventAddTodo(event) {
+    const { todo, listName } = event.detail;
+    if (listName === "Tasks") {
+      taskCards = [...taskCards, { todo }];
+    } else if (listName === "In Progress") {
+      inProgressCards = [...inProgressCards, { todo }];
+    } else {
+      doneCards = [...doneCards, { todo }];
+    }
+  }
 </script>
 
 <main class="container is-fluid">
   <h1 class="is-size-3">Sveltdo</h1>
   <div class="columns">
-    <CardList cards={taskCards} listName={"Tasks"} />
-    <CardList cards={inProgressCards} listName={"In Progress"} />
-    <CardList cards={doneCards} listName={"Done"} />
+    <CardList cards={taskCards} listName={"Tasks"} on:addTodo={handleEventAddTodo} />
+    <CardList cards={inProgressCards} listName={"In Progress"} on:addTodo={handleEventAddTodo}/>
+    <CardList cards={doneCards} listName={"Done"} on:addTodo={handleEventAddTodo}/>
   </div>
 </main>
 
--- src/components/CardList.svelte
+++ src/components/CardList.svelte
@@ -1,25 +1,28 @@
 <script>
-    import TodoCard from "./TodoCard.svelte";
-    export let cards, listName
+  import { createEventDispatcher } from "svelte";
+  import TodoCard from "./TodoCard.svelte";
+  export let cards, listName;
 
-    let todo = ""
+  const dispatch = createEventDispatcher();
+
+  let todo = "";
 
   function addTodo() {
-    cards = [...cards, { todo, list: "Task"}]
+    dispatch("addTodo", { todo, listName });
   }
 </script>
 
 <div class="column is-4">
-    <div class="card has-background-light">
-      <div class="card-header">
-        <p class="card-header-title">{listName}</p>
-      </div>
-      <div class="card-content">
-        {#each cards as card}
-          <TodoCard content={card.todo} />
-        {/each}
-        <input type="text" class="input is-primary mb-2" bind:value={todo}/>
-        <button on:click={addTodo} class="button is-primary">Add</button>
-      </div>
+  <div class="card has-background-light">
+    <div class="card-header">
+      <p class="card-header-title">{listName}</p>
     </div>
+    <div class="card-content">
+      {#each cards as card}
+        <TodoCard content={card.todo} />
+      {/each}
+      <input type="text" class="input is-primary mb-2" bind:value={todo} />
+      <button on:click={addTodo} class="button is-primary">Add</button>
+    </div>
+  </div>
 </div>
